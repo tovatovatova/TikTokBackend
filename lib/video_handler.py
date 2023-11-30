f217e4699e4350c01254418eb67cf11dc2fa3da3
Yechiel Wiesel <ywiesel@nvidia.com>
2023-11-30 21:24:35 +0200

    add to video handler video analyzing

--- lib/video_handler.py
+++ lib/video_handler.py
@@ -1,42 +1,48 @@
 import os
 import cv2
-from lib.openai_client import client
+import base64
+from openai_client import client
 
-def video_to_frames(file_path: str, frames_interval: int = 10) -> list[str]:
-    # Create frames directory if it doesn't exist
-    if not os.path.exists('frames'):
-        os.makedirs('frames')
+def video_to_frames(file_path: str, frames_interval: int = 60):
+    video = cv2.VideoCapture(file_path)
 
-    # Create a VideoCapture object
-    cap = cv2.VideoCapture(file_path)
+    base64Frames = []
+    frame_index = 0
+    while video.isOpened():
+        success, frame = video.read()
 
-    # Check if video opened successfully
-    if (cap.isOpened()== False): 
-        print("Error opening video file")
-
-    # Read until video is completed
-    frame_count = 0
-    frames = []
-    while(cap.isOpened()):
-        # Capture frame-by-frame
-        ret, frame = cap.read()
-        if ret == True:
-            # Save every frames_interval frame
-            if frame_count % frames_interval == 0:
-                frame_file_path = f"frames/frame_{frame_count}.jpg"
-                cv2.imwrite(frame_file_path, frame)
-                print(f"Saved frame {frame_count} at {frame_file_path}")
-                frames.append(frame_file_path)
-            frame_count += 1
-        # Break the loop if no more frames
-        else: 
+        if not success:
             break
 
-    # When everything done, release the video capture object
-    cap.release()
-
-    return frames
+        if frame_index % frames_interval == 0:
+            _, buffer = cv2.imencode(".jpg", frame)
+            base64Frames.append(base64.b64encode(buffer).decode("utf-8"))
+        frame_index += 1
+    video.release()
+    print(len(base64Frames), "frames read.")
+    return base64Frames
 
 ProblematicFrame = tuple[str, str] # frame path and the problem 
 def check_video_content(file_path: str) -> list[ProblematicFrame]:
-    ...
+    frames = video_to_frames(file_path)
+    PROMPT_MESSAGES = [
+        {
+            "role": "user",
+            "content": [
+                # "These are frames from a video that I want to upload. Generate a compelling description that I can upload along with the video.",
+                "Can tiktok approve that content?",
+                *map(lambda x: {"image": x, "resize": 768}, frames),
+            ],
+        },
+    ]
+    params = {
+        "model": "gpt-4-vision-preview",
+        "messages": PROMPT_MESSAGES,
+        "max_tokens": 200,
+    }
+
+    result = client.chat.completions.create(**params)
+    print(result.choices[0].message.content)
+
+if __name__ == '__main__':
+    check_video_content('C:/Users/ywiesel/Downloads/tovi-7.mp4')
