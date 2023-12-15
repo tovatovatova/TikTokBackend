4cb0b4edd1e7e47f16cd625ab1f04db1a94168a1
Yechiel Vizel <ywiesel@nvidia.com>
2023-12-15 21:02:43 +0200

    Implement the run_assistant func

--- lib/tools/openai_client.py
+++ lib/tools/openai_client.py
@@ -2,7 +2,7 @@ import os
 from enum import Enum
 
 from openai import OpenAI
-
+from openai.types.beta.threads import MessageContentText
 _API_KEY = os.environ['OPENAI_KEY']
 client = OpenAI(api_key=_API_KEY)
 
@@ -19,8 +19,12 @@ def run_assistant(assistant: Assistant, data: str) -> str:
     while run.status != 'completed':
         run = client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
     messages = client.beta.threads.messages.list(thread_id=thread.id)
-    print([(message.role, message.content) for message in messages.data])
-    raise NotImplementedError('need to return the actual value')
+    # unpacking like this so it's will error in case there is multiple
+    assist_message, = (message.content for message in messages.data if message.role == 'assistant')
+    content, = assist_message
+    # asserting that this is a message content (which has the text attr not like in image content for example..)
+    assert isinstance(content, MessageContentText)
+    return content.text.value
 
 
 def speech_to_text_srt(file_path: str, lang: str):
@@ -58,3 +62,57 @@ def send_images(base64_frames: list[str], prompt: str, format: str = 'image/jpeg
     assert result.choices[0].message.content is not None
 
     return result.choices[0].message.content
+
+
+if __name__ == '__main__':
+    res = run_assistant(Assistant.Text, """[
+    {
+        "idx": 1,
+        "text": "HAMAS PRODUCTION",
+        "score": null,
+        "reason": null
+    },
+    {
+        "idx": 2,
+        "text": "Now.",
+        "score": null,
+        "reason": null
+    },
+    {
+        "idx": 3,
+        "text": "Gently open the vehicle door.",
+        "score": null,
+        "reason": null
+    },
+    {
+        "idx": 4,
+        "text": "DIRECTOR'S CUT CWITH A KNIFE)",
+        "score": null,
+        "reason": null
+    },
+    {
+        "idx": 5,
+        "text": "TAKE 4",
+        "score": null,
+        "reason": null
+    },
+    {
+        "idx": 6,
+        "text": "HOSTAGE",
+        "score": null,
+        "reason": null
+    },
+    {
+        "idx": 7,
+        "text": "RELEASE",
+        "score": null,
+        "reason": null
+    },
+    {
+        "idx": 8,
+        "text": "PRESENTS:",
+        "score": null,
+        "reason": null
+    }
+]""")
+    print()
